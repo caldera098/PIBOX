be94bd8e43456e35652d1791ecf2f0429c9ff21d
caldera098 <36611038+caldera098@users.noreply.github.com>
2018-02-18 21:06:37 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
+sudo apt-get install shairport-sync nodejs libavahi-compat-libdnssd-dev mosquitto
+sudo npm install -g --unsafe-perm homebridge node-red node-red-node-serialport
+sudo cp Boots/* /etc/init.d
+sudo chmod +x /etc/init.d/homebridge /etc/init.d/node-red
+sudo update-rc.d homebridge defaults
+sudo update-rc.d node-red defaults
+sudo cp raspotify /etc/default
+sudo cp shairport-sync.conf /etc
+
