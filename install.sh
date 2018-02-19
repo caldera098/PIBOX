267f5dffa2dcba6651d24ce22db1fd18dfd660d2
caldera098 <36611038+caldera098@users.noreply.github.com>
2018-02-19 03:35:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 #!bin/bash
-
-sudo apt-get update
-sudo apt-get upgrade
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
-sudo apt-get install shairport-sync nodejs libavahi-compat-libdnssd-dev mosquitto
-sudo npm install -g --unsafe-perm homebridge node-red node-red-node-serialport
-sudo cp Boots/* /etc/init.d
-sudo chmod +x /etc/init.d/homebridge /etc/init.d/node-red
-sudo update-rc.d homebridge defaults
-sudo update-rc.d node-red defaults
-sudo cp raspotify /etc/default
-sudo cp shairport-sync.conf /etc
-sudo reboot
-
+echo "    #Attemping APT Update";
+sudo apt-get update;
+echo "    #Attemping APT Upgrade";
+sudo apt-get upgrade;
+echo "    #Adding Node to RPi Repository";
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -;
+echo "    #Installing Raspotify";
+curl -sL https://dtcooper.github.io/raspotify/install.sh | sh;
+echo "    #Installing Shairport-sync, Node, Mosquitto and LibAvahi";
+sudo apt-get install shairport-sync nodejs libavahi-compat-libdnssd-dev mosquitto;
+echo "    #Installing HomeBridge, HomeBridge's PlugIns, Node-Red and Node-Red-Serial";
+sudo npm install -g --unsafe-perm homebridge node-red node-red-node-serialport;
+echo "    #Making HomeBridge and Node-Red booteable";
+sudo cp ~/PiSmartBox/Boots/* /etc/init.d;
+sudo chmod +x /etc/init.d/homebridge /etc/init.d/node-red;
+sudo update-rc.d homebridge defaults;
+sudo update-rc.d node-red defaults;
+echo "    #Configuring HomeBridge";
+sudo cp ~/PiSmartBox/config.json ~/.homebridge
+echo "    #Configuring Raspotify's and Shairport-Sync's outputs";
+sudo cp ~/PiSmartBox/raspotify /etc/default;
+sudo cp ~/PiSmartBox/shairport-sync.conf /etc;
+echo "   ---------ALL DONE----------    ";
+sudo reboot;
