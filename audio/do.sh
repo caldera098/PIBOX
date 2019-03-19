3670b4b2440dc533bc9b86267c10a143a5e2d0d3
caldera098 <36611038+caldera098@users.noreply.github.com>
2019-03-19 06:03:23 -0300

    Update do.sh

--- audio/do.sh
+++ audio/do.sh
@@ -1,1 +1,27 @@
-
+#!/bin/bash
+VALUE=$(cat /home/pi/.audio/value)
+IN=$(cat /home/pi/.audio/income)
+if [ "$IN" -eq '2' ];
+  then
+    if [ "$VALUE" -le '95' ];
+     then
+      let VALUE=$VALUE+5
+      amixer sset 'Master' ${VALUE}%
+      $(echo $VALUE > /home/pi/.audio/value)
+      else
+    amixer sset 'Master' 100%
+    let $VALUE=100
+    fi
+fi
+if [ "$IN" -eq '1' ];
+  then
+    if [ "$VALUE" -ge '5' ];
+     then
+      let VALUE=$VALUE-5
+      amixer sset 'Master' ${VALUE}%
+      else
+      amixer sset 'Master' 0%
+      let VALUE=0
+    fi
+fi
+      $(echo $VALUE > /home/pi/.audio/value)
